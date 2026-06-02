f90ddc92883ba8e56a66c957a9b88e8443f5d477
sana <sana.zouaoui@esme.fr>
2026-06-02 16:14:31 +0200

    Ajout Docker et configuration PostgreSQL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5001",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5001",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Intercepteur pour les erreurs
 api.interceptors.response.use(
   (response) => response,
   (error) => {
